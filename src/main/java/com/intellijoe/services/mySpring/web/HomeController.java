858d2dc45549efa7ae524cd45ba0e1a9654500eb
victorygainer@gmail.com <victorygainer@gmail.com>
2023-03-10 17:43:31 +0900

    update build.gradle

---
+++ src/main/java/com/intellijoe/services/mySpring/web/HomeController.java
@@ -0,0 +1,13 @@
+package com.intellijoe.services.mySpring.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/hello") //
+    public String hello() {
+        return "hello";
+    }
+}
