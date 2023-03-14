aae95ab29c7a905a5d744d0395b0c7591e0561bf
victorygainer@gmail.com <victorygainer@gmail.com>
2023-03-14 22:35:13 +0900

    - Create/Update/Inquiry Registration API

--- src/main/java/com/intellijoe/services/mySpring/service/posts/PostsService.java
+++ src/main/java/com/intellijoe/services/mySpring/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.intellijoe.services.mySpring.service.posts;
 
+import com.intellijoe.services.mySpring.domain.posts.Posts;
 import com.intellijoe.services.mySpring.domain.posts.PostsRepository;
+import com.intellijoe.services.mySpring.web.dto.PostsResponseDto;
 import com.intellijoe.services.mySpring.web.dto.PostsSaveRequestDto;
+import com.intellijoe.services.mySpring.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,5 +18,17 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
     }
 }
