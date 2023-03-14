0f9b5a2120acd9aa6c4bf09a1f4e17d4c3b1060d
victorygainer@gmail.com <victorygainer@gmail.com>
2023-03-14 22:35:20 +0900

    Automate creation/modification time with JPA Auditing

--- src/main/java/com/intellijoe/services/mySpring/domain/posts/Posts.java
+++ src/main/java/com/intellijoe/services/mySpring/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.intellijoe.services.mySpring.domain.posts;
 
+import com.intellijoe.services.mySpring.domain.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/com/intellijoe/services/mySpring/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/intellijoe/services/mySpring/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,6 +22,28 @@ public class PostsRepositoryTest {
     @After
     public void cleanup() {
         postsRepository.deleteAll();
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2023,3,16,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>>>>>>>> createDate = " + posts.getCreatedDate() + ", modifiedDate = " + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+
     }
 
     @Test
@@ -43,5 +66,7 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+
+
     }
 }
