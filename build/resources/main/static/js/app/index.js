7ea17303d019892b3b71bb787c8ba6df8cb27385
victorygainer@gmail.com <victorygainer@gmail.com>
2023-03-17 16:21:47 +0900

    create update & delete page

--- build/resources/main/static/js/app/index.js
+++ build/resources/main/static/js/app/index.js
@@ -5,6 +5,12 @@ let main = {init : function () {
         $('#btn-save').on('click', function () {
             _this.save();
         });
+        $('#btn-update').on('click', function () {
+            _this.update();
+        });
+        $('#btn-delete').on('click', function () {
+            _this.delete();
+        });
     }, save : function () {
         let data = {
             title: $('#title').val(),
@@ -24,6 +30,42 @@ let main = {init : function () {
         }).fail(function (error){
             alert(JSON.stringify(error))
         });
-    }};
+    }, update : function () {
+        let data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        let id = $('#id').val();
+
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function (){
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error){
+            alert(JSON.stringify(error))
+        });
+    }, delete : function () {
+        let id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+        }).done(function (){
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error){
+            alert(JSON.stringify(error))
+        });
+    }
+};
 
 main.init();
--- src/main/java/com/intellijoe/services/mySpring/web/IndexController.java
+++ src/main/java/com/intellijoe/services/mySpring/web/IndexController.java
@@ -1,18 +1,33 @@
 package com.intellijoe.services.mySpring.web;
 
+import com.intellijoe.services.mySpring.service.posts.PostsService;
+import com.intellijoe.services.mySpring.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+    private final PostsService postsService;
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
     @GetMapping("/posts/save")
     public String save() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+        return "posts-update";
     }
 }
