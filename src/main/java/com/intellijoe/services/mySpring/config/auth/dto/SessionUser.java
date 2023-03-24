5f4ddcd88d2dc03ab3291885fd68e7a4bd12357e
victorygainer@gmail.com <victorygainer@gmail.com>
2023-03-24 23:27:46 +0900

    Using a database as a JDBC session store

--- src/main/java/com/intellijoe/services/mySpring/config/auth/dto/SessionUser.java
+++ src/main/java/com/intellijoe/services/mySpring/config/auth/dto/SessionUser.java
@@ -3,13 +3,15 @@ package com.intellijoe.services.mySpring.config.auth.dto;
 import com.intellijoe.services.mySpring.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
 
-    public SessionUser(User user) {
+    public SessionUser(User user)   {
         this.name = user.getName();
         this.email = user.getEmail();
         this.picture = user.getPicture();
--- src/main/java/com/intellijoe/services/mySpring/web/IndexController.java
+++ src/main/java/com/intellijoe/services/mySpring/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.intellijoe.services.mySpring.web;
 
+import com.intellijoe.services.mySpring.config.auth.LoginUser;
 import com.intellijoe.services.mySpring.service.posts.PostsService;
 import com.intellijoe.services.mySpring.web.dto.PostsResponseDto;
 import com.intellijoe.services.mySpring.config.auth.dto.SessionUser;
@@ -17,9 +18,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
         if (user != null){
             model.addAttribute("userName", user.getName());
         }
