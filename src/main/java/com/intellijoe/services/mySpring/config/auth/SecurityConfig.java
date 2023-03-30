8bc07504b7b171c1ff6c6df75fd89b96f02909c1
victorygainer@gmail.com <victorygainer@gmail.com>
2023-03-30 15:28:13 +0900

    EC2 Project Settings

--- src/main/java/com/intellijoe/services/mySpring/config/auth/SecurityConfig.java
+++ src/main/java/com/intellijoe/services/mySpring/config/auth/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -18,7 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration {
+public class SecurityConfig {
     private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
