4750d314d972e69ea57d8072ccb4f581b8535a2d
victorygainer@gmail.com <victorygainer@gmail.com>
2023-04-05 14:36:10 +0900

    CodeDeploy Deployment Settings

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ PROJECT_NAME=springboot-webservice-myspring
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/home/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
