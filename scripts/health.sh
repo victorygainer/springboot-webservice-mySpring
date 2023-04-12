36645261a02ff055d7d86de1ec91b19122a24cb6
victorygainer@gmail.com <victorygainer@gmail.com>
2023-04-12 16:33:33 +0900

    Create a non-disruptive deployment

--- scripts/health.sh
+++ scripts/health.sh
@@ -5,7 +5,7 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 source ${ABSDIR}/switch.sh
 
-IDLE_PORT=$(find_idle_port)
+IDLE_PORT=$(curl -s http://localhost:8080/actuator/shutdown | grep -o '[0-9]*' | head -1)
 
 echo "> Health Check Start!"
 echo "> IDLE_PORT: $IDLE_PORT"
@@ -15,12 +15,12 @@ sleep 10
 for RETRY_COUNT in {1..10}
 do
   RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
-  UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -1)
+  UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
 
   if [ ${UP_COUNT} -ge 1 ]
-  then # $up_count >= 1 ("real 문자열이 있는지 검증)
+  then # $up_count >= 1 ("real" 문자열이 있는지 검증)
     echo "> Health check 성공"
-    switch_proxy
+    source ${ABSDIR}/switch.sh
     break
   else
     echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
