411a46b20d0e68f19d39e3dd39c8fbb731629c48
victorygainer@gmail.com <victorygainer@gmail.com>
2023-04-12 15:24:55 +0900

    Create a non-disruptive deployment

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,29 +4,16 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-REPOSITORY=/home/ec2-user/app/step3
-PROJECT_NAME=springboot-webservice-mySpring
+IDLE_PORT=$(find_idle_port)
 
-echo "> Build 파일복사"
-echo "> $REPOSITORY/zip/*.jar $REPOSITORY/"
+echo "> $IDLE_PORT에서 구동 중인 애플리케이션 pid 확인"
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
-
-echo "> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-
-echo "> JAR Name: $JAR_NAME"
-
-echo "> $JAR_NAME에 실행권한 추가"
-
-chmod +x $JAR_NAME
-
-echo "> $JAR_NAME 실행"
-
-IDLE_PROFILE=$(find_idle_profile)
-
-echo "> $JAR_NAME를 profile=$IDLE_PROFILE 로 실행합니다"
-nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties, classpath:/application-$IDLE_PROFILE.properties, /home/ec2-user/app/application-oauth.properties, /home/ec2-user/app/application-real-db.properties \
-  -Dspring.profiles.active=$IDLE_PROFILE \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+if [ -z ${IDLE_PID} ]
+then
+  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo ">kill -15 $IDLE_PID"
+  kill -15 ${IDLE_PID}
+  sleep5
+fi
