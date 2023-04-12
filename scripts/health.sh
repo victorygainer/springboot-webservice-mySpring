cc4e43fd224c93b56fd7104b3c32db20341b69ef
victorygainer@gmail.com <victorygainer@gmail.com>
2023-04-12 16:48:34 +0900

    Create a non-disruptive deployment

--- scripts/health.sh
+++ scripts/health.sh
@@ -5,7 +5,7 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 source ${ABSDIR}/switch.sh
 
-IDLE_PORT=$(curl -s http://localhost:8080/actuator/shutdown | grep -o '[0-9]*' | head -1)
+IDLE_PORT=$(find_idle_port)
 
 echo "> Health Check Start!"
 echo "> IDLE_PORT: $IDLE_PORT"
@@ -18,9 +18,9 @@ do
   UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
 
   if [ ${UP_COUNT} -ge 1 ]
-  then # $up_count >= 1 ("real" 문자열이 있는지 검증)
+  then # $up_count >= 1 ("real 문자열이 있는지 검증)
     echo "> Health check 성공"
-    source ${ABSDIR}/switch.sh
+    switch_proxy
     break
   else
     echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
